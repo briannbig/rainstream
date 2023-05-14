31e75ba500de9b77af6aefd7fe618497357da1f8
Brian Barasa <briannbig@gmail.com>
2023-05-14 08:59:49 +0300

    initial commit

---
+++ rainstream_java/src/test/java/brianbig/Rainstream_javaTest.java
@@ -0,0 +1,21 @@
+package brianbig;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Rainstream_javaTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
