e36653584655e0f58a12976fa9429b8df251cd42
Brian Barasa <briannbig@gmail.com>
2023-05-15 08:07:10 +0300

    simplify generating weather info

--- rainstream_java/src/main/java/brianbig/WeatherEndpoint.java
+++ rainstream_java/src/main/java/brianbig/WeatherEndpoint.java
@@ -3,44 +3,46 @@ package brianbig;
 
 import brianbig.gen.WeatherStream;
 import brianbig.gen.WeatherStreamServiceGrpc;
+import com.github.javafaker.Faker;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class WeatherEndpoint extends WeatherStreamServiceGrpc.WeatherStreamServiceImplBase {
 
-    final WeatherEngine weatherEngine;
+    final Faker faker = new Faker();
 
-    @Inject
-    public WeatherEndpoint(WeatherEngine weatherEngine) {
-        this.weatherEngine = weatherEngine;
-    }
+
+
 
     @Override
     public void getInfoOnce(WeatherStream.WeatherRequest request, StreamObserver<WeatherStream.WeatherResponse> responseObserver) {
-        responseObserver.onNext(weatherEngine.createInfo());
+        responseObserver.onNext(createInfo());
 
         responseObserver.onCompleted();
     }
 
     @Override
     public void getInfoContinuously(WeatherStream.WeatherRequest request, StreamObserver<WeatherStream.WeatherResponse> responseObserver) {
-        weatherEngine.rainResponseStream.limit(request.getStreamLimit()).forEach(
-                rainResponse -> {
-                    try {
-                        Thread.sleep(1000);
-                        responseObserver.onNext(rainResponse);
 
-                    } catch (InterruptedException e) {
-                        responseObserver.onError(Status.INTERNAL.withCause(e).asRuntimeException());
-                    }
+        for (int i = 0; i < request.getStreamLimit(); i++) {
+            try{
+                Thread.sleep(1000);
+            responseObserver.onNext(createInfo());} catch (InterruptedException e) {
+                responseObserver.onError(Status.INTERNAL.withCause(e).asRuntimeException());
+            }
 
-                }
-        );
+        }
         responseObserver.onCompleted();
     }
 
 
+    private WeatherStream.WeatherResponse createInfo() {
+        return WeatherStream.WeatherResponse.newBuilder()
+                .setCity(faker.country().capital())
+                .setInfo(faker.weather().description())
+                .build();
+
+    }
 }
